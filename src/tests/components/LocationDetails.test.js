4750a5ca66fcbab13b4f6eff6f6674d59c2cd392
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-16 16:14:59 +0000

    Amended the test module to include .toHaveClass('location-details')

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -7,6 +7,6 @@ describe('ForecastDetails', () => {
     const { getByText } = render(
       <LocationDetails city="Manchester" country="UK" />
     );
-    expect(getByText('Manchester, UK')).toBeTruthy();
+    expect(getByText('Manchester, UK')).toHaveClass('location-details');
   });
 });
