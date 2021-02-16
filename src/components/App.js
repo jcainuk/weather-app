e2ced0dd9a3864d8db078da0acdb3b7f892ba167
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-16 15:30:11 +0000

    Imported PropTypes on App.js and Add PropTypes validation for the location prop in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import '../styles/App.css';
+import PropTypes from 'prop-types';
 import LocationDetails from './LocationDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <LocationDetails />
-    </div>
-  );
-}
+const App = ({ location }) => {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
+};
 
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 export default App;
