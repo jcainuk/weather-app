0629ee8c4a33f6c592c4f6d3e17ccf2e2359c695
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-16 16:04:52 +0000

    Wrote first test assertion and passed test

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -3,5 +3,10 @@ import { render } from '@testing-library/react';
 import LocationDetails from '../../components/LocationDetails';
 
 describe('ForecastDetails', () => {
-  it('renders the correct city and location props', () => {});
+  it('renders the correct city and location props', () => {
+    const { getByText } = render(
+      <LocationDetails city="Manchester" country="UK" />
+    );
+    expect(getByText('Manchester, UK')).toBeTruthy();
+  });
 });
