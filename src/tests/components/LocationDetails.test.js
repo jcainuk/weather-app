80c12438949177f03fbc456dc50c45720dabef8d
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-16 16:01:48 +0000

    Wrote first describe block on LocationDetails test module

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import LocationDetails from '../../components/LocationDetails';
+
+describe('ForecastDetails', () => {
+  it('renders the correct city and location props', () => {});
+});
