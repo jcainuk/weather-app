1942f9ec61e38ee13bd5977d21be210714bc0925
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-20 15:25:50 +0000

    Added getByTestId to methods accessible from render() and modified assertion in ForecastSummary.test.js file. Passed the right value to getByTestId()

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -25,7 +25,7 @@ describe('ForecastSummary', () => {
     expect(asFragment()).toMatchSnapshot();
   });
   it('renders correct values for props', () => {
-    const { getByText } = render(
+    const { getByText, getByTestId } = render(
       <ForecastSummary
         date={validProps.date}
         description={validProps.description}
@@ -38,7 +38,7 @@ describe('ForecastSummary', () => {
     expect(getByText('Stub description')).toHaveClass(
       'forecast-summary__description'
     );
-    expect(getByText('stubIcon')).toHaveClass('forecast-summary__icon');
+    expect(getByTestId('forecast-icon')).toHaveClass('forecast-summary__icon');
     expect(getByText('22°C')).toHaveClass('forecast-summary__temperature');
   });
 });
