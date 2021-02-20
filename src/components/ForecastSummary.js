ab41c9077cd290922853df1185cc675f9b44c4a6
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-20 12:53:51 +0000

    Deconstructed and validated props in ForecastSummary component

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ForecastSummary = () => {
+const ForecastSummary = (props) => {
+  const { date, description, icon, temperature } = props;
   return <div className="forecast-summary" />;
 };
 
 export default ForecastSummary;
+
+ForecastSummary.propTypes = {
+  date: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  temperature: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+  }).isRequired,
+};
