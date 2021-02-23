9dff1f8d57191843346967358e16bff14bbfbcbb
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-23 16:18:41 +0000

    Added an imported css stylesheets for App.js and ForecastSummaries.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from './LocationDetails';
 import ForecastSummaries from './ForecastSummaries';
+import '../styles/App.css';
 
 const App = ({ location, forecasts }) => (
-  <div className="forecast">
+  <div className="weather-app">
     <LocationDetails city={location.city} country={location.country} />
 
     <ForecastSummaries forecasts={forecasts} />
