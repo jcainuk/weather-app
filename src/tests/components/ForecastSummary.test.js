9f8ab016178fea742cf6ab2044394bbff98dcb5b
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-23 14:43:43 +0000

    Wrote tests for ForecastSummaries but failing

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -4,26 +4,30 @@ import ForecastSummary from '../../components/ForecastSummary';
 
 describe('ForecastSummary', () => {
   const validProps = {
-    date: 1111111,
+    date: 11111111,
     description: 'Stub description',
-    icon: 'stubIcon',
+    icon: '800',
     temperature: {
       min: 12,
       max: 22,
     },
+    onSelect: () => {},
   };
 
-  it('renders correctly', () => {
+  it('renders as expected', () => {
     const { asFragment } = render(
       <ForecastSummary
         date={validProps.date}
         description={validProps.description}
         icon={validProps.icon}
         temperature={validProps.temperature}
+        onSelect={validProps.onSelect}
       />
     );
+
     expect(asFragment()).toMatchSnapshot();
   });
+
   it('renders correct values for props', () => {
     const { getByText, getByTestId } = render(
       <ForecastSummary
@@ -31,10 +35,11 @@ describe('ForecastSummary', () => {
         description={validProps.description}
         icon={validProps.icon}
         temperature={validProps.temperature}
+        onSelect={validProps.onSelect}
       />
     );
 
-    expect(getByText('1111111')).toHaveClass('forecast-summary__date');
+    expect(getByText('Thu 1st Jan')).toHaveClass('forecast-summary__date');
     expect(getByText('Stub description')).toHaveClass(
       'forecast-summary__description'
     );
