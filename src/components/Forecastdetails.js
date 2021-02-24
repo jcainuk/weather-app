fd324e9dbffb3a2883e5beab2bf93d6693f3a700
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-24 15:23:54 +0000

    Added prop to ForecastDetails component and created new testing component

--- src/components/Forecastdetails.js
+++ src/components/Forecastdetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ForecastDetails = (props) => {
+  const { forecast } = props;
   return <div className="forecast-details" data-testid="forecast-details" />;
 };
 
