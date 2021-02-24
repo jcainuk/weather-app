52059ec3299f623a641326ac992137674cb9cfca
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-24 15:17:44 +0000

    Created ForecastDetails component

---
+++ src/components/Forecastdetails.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ForecastDetails = (props) => {
+  return <div className="forecast-details" data-testid="forecast-details" />;
+};
+
+export default ForecastDetails;
