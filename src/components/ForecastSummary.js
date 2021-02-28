9084e9750af7dc355f6a91d43e3d848bd66e7a70
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-28 13:19:02 +0000

    Passed onSelect prop to ForecastSummary and ForecastSummaries components

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -5,7 +5,7 @@ import WeatherIcon from 'react-icons-weather';
 import moment from 'moment';
 
 const ForecastSummary = (props) => {
-  const { date, description, icon, temperature } = props;
+  const { date, description, icon, temperature, onSelect } = props;
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">
@@ -36,4 +36,5 @@ ForecastSummary.propTypes = {
     min: PropTypes.number,
     max: PropTypes.number,
   }).isRequired,
+  onSelect: PropTypes.func.isRequired,
 };
