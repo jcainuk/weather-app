a330243495ebe6523e15202fa4f494eedf43a028
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-28 15:38:36 +0000

    Called getForecast() in useEffect() hook. Set the values of forecasts, location, and selectedDate appropriately within getForecast() request.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import '../styles/App.css';
+import axios from 'axios';
 import LocationDetails from './LocationDetails';
 import ForecastSummaries from './ForecastSummaries';
 import ForecastDetails from './ForecastDetails';
-import '../styles/App.css';
-import axios from ('axios');
 
-const App = ({ forecasts, location }) => {
-  const [forecasts, setForecasts] = useState([0]);
-  const [location, setLocation] = useState({ city: "", country: "" });
-  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+const App = () => {
+  const [forecasts, setForecasts] = useState([]);
+  const [location, setLocation] = useState({ city: '', country: '' });
+  const [selectedDate, setSelectedDate] = useState(0);
+
+  const getForecast = () => {
+    const endpoint = 'https://mcr-codes-weather-app.herokuapp.com/forecast';
+
+    axios.get(endpoint).then((response) => {
+      setSelectedDate(response.data.forecasts[0].date);
+      setForecasts(response.data.forecasts);
+      setLocation(response.data.location);
+    });
+  };
+
+  useEffect(() => {
+    getForecast(setSelectedDate, setForecasts, setLocation);
+  }, []);
+
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
   );
@@ -24,10 +39,9 @@ const App = ({ forecasts, location }) => {
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
       />
-      {selectedForecast && (<ForecastDetails forecast={selectedForecast} />)}
+      {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
     </div>
   );
 };
-
 
 export default App;
