330b7660f924f6d046df6bec899ea5f3890b6668
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-28 14:29:19 +0000

    Removed props import and props validation from App component and used Use useState() hook to create forecasts and location

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import LocationDetails from './LocationDetails';
 import ForecastSummaries from './ForecastSummaries';
 import ForecastDetails from './ForecastDetails';
 import '../styles/App.css';
 
 const App = ({ forecasts, location }) => {
+  const [forecasts, setForecasts] = useState([]);
+  const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
@@ -27,21 +28,5 @@ const App = ({ forecasts, location }) => {
   );
 };
 
-App.propTypes = {
-  forecasts: PropTypes.arrayOf(
-    PropTypes.shape({
-      date: PropTypes.number,
-      description: PropTypes.string,
-      icon: PropTypes.string,
-      temperature: PropTypes.shape({
-        max: PropTypes.number,
-        min: PropTypes.number,
-      }),
-    })
-  ).isRequired,
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string,
-  }).isRequired,
-};
+
 export default App;
