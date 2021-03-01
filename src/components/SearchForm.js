e3d4efb9ae33474e4533777a608f2e60714ac4a6
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-03-01 15:03:31 +0000

    Made handleCitySearch() available to the "Search" button in <SearchForm />. Passed handleCitySearch() as onSubmit prop to <SearchForm /> and validated it, set it as the onClick event handler for "Search" button

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SearchForm = ({ searchText, setSearchText }) => {
+const SearchForm = ({ searchText, setSearchText, onSubmit }) => {
   const handleInputChange = (event) => setSearchText(event.target.value);
   return (
     <div className="search-form">
       <input type="text" onChange={handleInputChange} value={searchText} />
-      <button type="submit">Search</button>
+      <button type="submit" onClick={onSubmit}>
+        Search
+      </button>
     </div>
   );
 };
@@ -16,4 +18,5 @@ export default SearchForm;
 SearchForm.propTypes = {
   searchText: PropTypes.string.isRequired,
   setSearchText: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
