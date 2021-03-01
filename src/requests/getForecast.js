e1c98e232cfbe4a981c9c1d74b06153a5060b577
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-03-01 14:59:46 +0000

    modified endpoint  in getForecast so it has a city param when searchText holds value

--- src/requests/getForecast.js
+++ src/requests/getForecast.js
@@ -6,9 +6,13 @@ const getForecast = (
   setForecasts,
   setLocation
 ) => {
-  const endpoint = 'https://mcr-codes-weather-app.herokuapp.com/forecast';
+  let endpoint = 'https://mcr-codes-weather-app.herokuapp.com/forecast';
 
-  axios.get(endpoint).then((response) => {
+  if (searchText) {
+    endpoint += `?city=${searchText}`;
+  }
+
+  return axios.get(endpoint).then((response) => {
     setSelectedDate(response.data.forecasts[0].date);
     setForecasts(response.data.forecasts);
     setLocation(response.data.location);
